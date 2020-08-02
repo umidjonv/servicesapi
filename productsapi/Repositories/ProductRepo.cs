5f1f4a89045341fd07c5e4eb6a11ba23fa617957
Umidjon Vakhidov <umidjon.vakhidov@mail.ru>
2020-08-02 19:15:33 +0500

    Add project files.

---
+++ productsapi/Repositories/ProductRepo.cs
@@ -0,0 +1,28 @@
+﻿using productsapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productsapi.Repositories
+{
+    public class ProductRepo : IProduct
+    {
+        public List<Product> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product getOneByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid addNew(Product product) 
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
