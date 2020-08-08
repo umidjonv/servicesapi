9747304545896bff9a9eb0ff046be038ebd65ae4
Umidjon Vakhidov <umidjon.vakhidov@mail.ru>
2020-08-08 12:53:30 +0500

    add audit entity

---
+++ productsapi/Entities/Audit/EntityLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productsapi.Entities.Log
+{
+    public class EntityLog : IEntity
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        [DefaultValue(true)]
+        public bool status { get; set; }
+
+        public string table { get; set; }
+    }
+}
---
+++ productsapi/Entities/Audit/IEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productsapi.Entities
+{
+    public interface IEntity
+    {
+        
+        public int id { get; set; }
+
+
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        [DefaultValue(true)]
+        public bool status { get; set; }
+    }
+
+  
+}
